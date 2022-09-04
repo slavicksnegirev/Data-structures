51f943309d8491ef7f4cb63c845898c4c5221e6f
Slavick <92212345+slavicksnegirev@users.noreply.github.com>
2022-09-04 16:39:50 +0300

    Linked List

---
+++ LinkedList/Model/LinkedList.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections;
+
+namespace LinkedList.Model
+{
+    /// <summary>
+    /// Односвязный список.
+    /// </summary>
+    public class LinkedList<T> : IEnumerable
+    {
+        /// <summary>
+        /// Первый элемент списка.
+        /// </summary>
+        public Item<T> Head { get; private set; }
+
+        /// <summary>
+        /// Последний элемент списка.
+        /// </summary>
+        public Item<T> Tail { get; private set; }
+
+        /// <summary>
+        /// Количество элементов в списке.
+        /// </summary>
+        public int Count { get; private set; }
+
+        /// <summary>
+        /// Инициализация пустого списка.
+        /// </summary>
+        public LinkedList()
+        {
+            Clear();
+        }
+
+        /// <summary>
+        /// Инициализация списка с начальным элементом.
+        /// </summary>
+        /// <param name="data"></param>
+        public LinkedList(T data)
+        {
+            var item = new Item<T>(data);
+            SetHeadAndTail(item);
+        }
+
+        /// <summary>
+        /// Добавить данные в конец списка.
+        /// </summary>
+        /// <param name="data"></param>
+        public void Add(T data)
+        {
+            var item = new Item<T>(data);
+
+            if (Tail != null)
+            {
+                Tail.Next = item;
+                Tail = item;
+                Count++;
+            }
+            else
+            {
+                SetHeadAndTail(item);
+            }
+        }
+
+        /// <summary>
+        /// Добавить элемент в начало списка.
+        /// </summary>
+        /// <param name="data"></param>
+        public void AppToHead(T data)
+        {
+            var item = new Item<T>(data);
+
+            item.Next = Head;
+            Head = item;
+            Count++;
+        }
+
+        public void InsertAfter(T target, T data)
+        {
+            if (Head != null)
+            {            
+                var current = Head;
+
+                while (current != null)
+                {
+                    if (current.Data.Equals(target))
+                    {
+                        var item = new Item<T>(data);
+                        item.Next = current.Next;
+                        current.Next = item;
+                        Count++;
+                        return;
+                    }
+                    else
+                    {
+                        current = current.Next;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Удалить первое вхождение в данный список.
+        /// </summary>
+        /// <param name="data"></param>
+        public void Delete(T data)
+        {
+            if (Head != null)
+            {
+                if(Head.Data.Equals(data))
+                {
+                    Head = Head.Next;
+                    Count--;
+                    return;
+                }
+
+                var current = Head.Next;
+                var previous = Head;
+
+                while (current != null)
+                {
+                    if (current.Data.Equals(data))
+                    {
+                        previous.Next = current.Next;
+                        Count--;
+                        return;
+                    }
+                    else
+                    {
+                        previous = current;
+                        current = current.Next;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Очистить весь список.
+        /// </summary>
+        public void Clear()
+        {
+            Head = null;
+            Tail = null;
+            Count = 0;
+        }
+
+        /// <summary>
+        /// Получить перечисление всех элементов списка.
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerator GetEnumerator()
+        {
+            var current = Head;
+
+            while(current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+
+        private void SetHeadAndTail(Item<T> item)
+        {
+            Head = item;
+            Tail = item;
+            Count = 1;
+        }
+
+        public override string ToString()
+        {
+            return "Linked list.\nЭлементов в списке: " + Count + "\n"; 
+        }
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new Model.LinkedList<int>();
+
+            for (int i = 1; i < 6; i++)
+            {
+                list.Add(i);
+            }
+
+            foreach (var item in list)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            list.Delete(3);
+            list.Delete(1);
+            list.Delete(7);
+
+            foreach (var item in list)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            list.AppToHead(7);
+            foreach (var item in list)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            list.InsertAfter(4, 8);
+            foreach (var item in list)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
